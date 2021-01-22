3df41b06447e06596a0e10f36c28308ac56494d1
Dmitriy Tkachenko <dimamarksman@gmail.com>
2021-01-22 17:23:02 +0200

    example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'test-fundamental-ngx';
+  formGroupRegister: FormGroup;
+  submitted = false;
+
+  errorTextStream: BehaviorSubject<string> = new BehaviorSubject('Error!');
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.formGroupRegister = new FormGroup({});
+  }
+
+  hasError(): boolean {
+    return this.formGroupRegister.invalid && this.submitted;
+  }
+
+  onSubmit(): void {
+    // stop here if form is invalid
+    if (this.formGroupRegister.invalid) {
+      this.submitted = true;
+      return;
+    }
+
+    // display form values on success
+    alert(
+      'SUCCESS!! :-)\n\n' +
+        JSON.stringify(this.formGroupRegister.value, null, 4)
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { FundamentalNgxCoreModule } from '@fundamental-ngx/core';
+import { FundamentalNgxPlatformModule } from '@fundamental-ngx/platform';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FundamentalNgxCoreModule,
+    FundamentalNgxPlatformModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
